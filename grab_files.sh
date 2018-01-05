f8cd6c40f55304e8b21c66fc69097fb438d8c7bf
technetos <mrgould93@gmail.com>
2018-01-04 20:37:44 -0500

    updated with efi path

--- grab_files.sh
+++ grab_files.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+EFI_PATH="EFI/BOOT/"
+
 BASE_URL="rsync://mirrors.kernel.org/slackware/slackware64-current"
 EXCLUDES="
 --exclude=e
@@ -14,7 +16,5 @@ rsync -r -v $EXCLUDES $BASE_URL"/slackware64" .
 INSTALLER_KERNEL=$BASE_URL"/kernels/huge.s/bzImage"
 INSTALLER_RAMDISK=$BASE_URL"/isolinux/initrd.img"
 
-rsync -v $INSTALLER_KERNEL .
-rsync -v $INSTALLER_RAMDISK .
-
-wget http://fay.slackmagic.com/filedump/uefi/BOOTX64.EFI
+rsync -v $INSTALLER_KERNEL $EFI_PATH
+rsync -v $INSTALLER_RAMDISK $EFI_PATH
