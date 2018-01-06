41cefb121a349090c02ad3277fcf3b3fba75d32e
technetos <mrgould93@gmail.com>
2018-01-06 13:15:10 -0500

    updated to current version

--- grab_files.sh
+++ grab_files.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-EFI_PATH="EFI/BOOT/"
+EFI_PATH="EFI"
+ISOLINUX_PATH="isolinux"
+KERNELS_PATH="kernels"
 
 BASE_URL="rsync://mirrors.kernel.org/slackware/slackware64-current"
+
 EXCLUDES="
 --exclude=e
 --exclude=f
@@ -13,8 +16,10 @@ EXCLUDES="
 --exclude=y"
 rsync -r -v $EXCLUDES $BASE_URL"/slackware64" .
 
-INSTALLER_KERNEL=$BASE_URL"/kernels/huge.s/bzImage"
-INSTALLER_RAMDISK=$BASE_URL"/isolinux/initrd.img"
+INSTALLER_EFI=$BASE_URL/$EFI_PATH
+INSTALLER_ISOLINUX=$BASE_URL/$ISOLINUX_PATH
+INSTALLER_KERNELS=$BASE_URL/$KERNELS_PATH
 
-rsync -v $INSTALLER_KERNEL $EFI_PATH
-rsync -v $INSTALLER_RAMDISK $EFI_PATH
+rsync -r -v $INSTALLER_KERNELS .
+rsync -r -v $INSTALLER_ISOLINUX . 
+rsync -r -v $INSTALLER_EFI .
